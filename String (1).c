37b6a14ad46445c1f5526f922d4f974fc80bce32
Ashok Budha <ashok.budha2015@gmail.com>
2020-01-28 21:41:59 +0545

    Add files via upload

---
+++ String (1).c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int test;
+    scanf("%d",&test);
+    int t;
+    for(t=1;t<=test;t++)
+    {
+        int n,d=0,k;
+        scanf("%d",&n);
+        for(k=1;k<=n;k++)
+        {
+            int i,j,count = 0;
+            for(i=1;i<=cbrt(k);i++)
+            {
+                for(j=1;j<=cbrt(k);j++)
+                {
+                    if (pow(i,3) + pow(j,3)==k)
+                    {
+                        count = count +1;
+                    }
+                }
+            }
+            if (count == 4)
+            {
+                d=d+1;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        printf("%d\n",d);
+    }
+    return 0;
+}
